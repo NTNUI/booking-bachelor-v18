18c0ffdb5d311a0782798a562ccd2af397cde43f
magnuspl <magnuslangeland80@gmail.com>
2018-02-12 16:02:19 +0100

    fixed booking modelling error

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+import datetime
 
 # Create your models here.
 
@@ -6,7 +12,7 @@ class Booking(models.Model):
     date_issued = models.DateField(auto_now_add=True)
     booking_id = models.AutoField(primary_key=True)
     customer_number = models.ForeignKey(
-        'Customer',
+        'accounts.User',
         on_delete=models.CASCADE,
     )
     location_id = models.ForeignKey(
@@ -15,11 +21,9 @@ class Booking(models.Model):
     )
 
 class Location(models.Model):
-    location_id = models.ForeignKey(
-        'Location',
-        on_delete=models.CASCADE,
-    )
+    booking_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     slug = models.CharField(max_length=12)
     description = models.TextField(max_length=200)
+
 
