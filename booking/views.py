e0acde2481baa36bbc20eb1023bb8efca751adcd
Henrik Hind Sørensen <henrisor@stud.ntnu.no>
2018-02-14 10:21:31 +0100

    added login requirement for booking page

--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 
-
+@login_required
 def index(request):
 
     return render(request, 'booking/booking.html')
