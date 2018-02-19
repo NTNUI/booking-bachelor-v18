9dc4f427bafef895326303a28e8a49d84e0e25ec
Henrik Hind Sørensen <henrisor@stud.ntnu.no>
2018-02-19 13:53:59 +0100

    created location model and added foreign key to booking model

--- booking/models.py
+++ booking/models.py
@@ -2,16 +2,28 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
+#Location model
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
 #Booking model
 class Booking(models.Model):
     name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     contact_date = models.DateField(_(u"Date"), blank=True)
     contact_time = models.TimeField(_(u"Time"), blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('booking_edit', kwargs={'pk': self.pk})
+
+
+
 
