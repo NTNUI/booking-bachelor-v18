5a1daf468c63db75fb17c19f63780671f019f4b1
magnuspl <magnuslangeland80@gmail.com>
2018-02-19 10:11:13 +0100

    backend functionality for making bookings

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from booking.models import Booking
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+
 
 # Create your views here.
 
@@ -8,3 +14,21 @@ from django.http import HttpResponse
 def index(request):
 
     return render(request, 'booking/booking.html')
+
+
+class BookingList(ListView):
+    model = Booking
+
+class BookingCreate(CreateView):
+    model = Booking
+    success_url = reverse_lazy('booking_list')
+    fields = ['name', 'location', 'contact_date', 'contact_time']
+
+class BookingUpdate(UpdateView):
+    model = Booking
+    success_url = reverse_lazy('booking_list')
+    fields = ['name', 'location', 'contact_date', 'contact_time']
+
+class BookingDelete(DeleteView):
+    model = Booking
+    success_url = reverse_lazy('booking_list')
--- ntnui/settings.py
+++ ntnui/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django_nose',
     'hs',
     'forms',
+    'booking',
     'rules.apps.AutodiscoverRulesConfig',
 ]
 
