af83186d4e2f6032341aa2feae9caac5812e8efa
magnuspl <magnuslangeland80@gmail.com>
2018-02-19 18:03:43 +0100

    added person as foreignkey in booking model

--- booking/models.py
+++ booking/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 #Booking model
 class Booking(models.Model):
+    person = models.ForeignKey('accounts.User', on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     contact_date = models.DateField(_(u"Date"), blank=True)
