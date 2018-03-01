c1686f387dbb89182f02aa83657cb807589b914c
Henrik Hind Sørensen <henrisor@stud.ntnu.no>
2018-03-01 16:29:46 +0100

    added django-filter to requirements.txt and installer_apps

--- ntnui/settings.py
+++ ntnui/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters'
+    'django_filters',
     'ntnui',
     'widget_tweaks',
     'accounts',
