f14ef216cf634057626f98f3d6ed78b24e4763b3
Martin Aarvik <maraarvik@gmail.com>
2018-03-13 18:21:18 +0100

    hacky method to increment queueno if double booking

--- booking/models.py
+++ booking/models.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
+
 #Location model
 class Location(models.Model):
     name = models.CharField(max_length=200)
@@ -21,7 +22,7 @@ class Booking(models.Model):
     start = models.DateTimeField(_(u'Start'), blank=True)
     end = models.DateTimeField(_(u'End'), blank=True)
     description = models.CharField(max_length=400, default='')
-    queueNO = models.IntegerField(default=0)
+    queueNo = models.IntegerField(default=0)
 
     def __str__(self):
         return self.description
@@ -34,9 +35,13 @@ class Booking(models.Model):
         print(self.location)
         model = Booking
         bookings = model.objects.filter(location=self.location, start=self.start, end=self.end)
-        bookings = list(bookings)
-        if not bookings:
-            self.queueNo = max(bookings, lambda x: x.queueNo)+1
+        # bookings = list(bookings)
+        if list(bookings) != []:
+            # self.queueNo = max(bookings, (item.queueNo for item in bookings))
+            maxVal =  bookings.aggregate(models.Max('queueNo'))
+            temp = [maxVal [i] for i in sorted(maxVal.keys())]
+            self.queueNo = int(temp[0])+1
+            # self.queueNo += 1
             print(self.queueNo)
         print(bookings)
         return super(Booking, self).save(*args, **kwargs)
