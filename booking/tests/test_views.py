79a47f0946806f2521d711a8c45e0526a03e3c8b
Martin Aarvik <maraarvik@gmail.com>
2018-05-23 13:32:41 +0200

    selenium tests

--- booking/tests/test_views.py
+++ booking/tests/test_views.py
@@ -2,3 +2,31 @@ from django.test import TestCase, RequestFactory
 from accounts.models import User
 from booking.models import Booking, Location
 
+class test_views_func(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_error_404(self):
+        self.assertTrue(True)
+    
+    def test_index(self):
+        self.assertTrue(True)
+    
+    def test_booking_list(self):
+        self.assertTrue(True)
+    
+    def test_booking_manage(self):
+        self.assertTrue(True)
+    
+    def test_api(self):
+        self.assertTrue(True)
+    
+    def test_save_booking_form(self):
+        self.assertTrue(True)
+    
+    def test_booking_create(self):
+        self.assertTrue(True)
+    
+    def test_booking_create_from_calendar(self):
+        self.assertTrue(True)
